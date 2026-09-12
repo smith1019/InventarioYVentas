b5b1d8ba45edfdfada7ba9d68b7cccda15c3b83d
Anderson smith quintero mesa <asqm2003@gmail.com>
2026-09-12 08:33:07 -0500

    Agregar estilos CSS a la pantalla de login

--- frontend/login.php
+++ frontend/login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar sesion - InventarioYVentas</title>
+    <link rel="stylesheet" href="../assets/css/login.css">
 </head>
 <body>
     <h1>Iniciar sesion</h1>
